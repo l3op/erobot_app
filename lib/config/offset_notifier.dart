2847867589196419dfcf6a2da2fd2f58cba2b53a
theacheng <theacheng.g6@gmail.com>
2020-09-10 10:00:02 +0700

    update transition

--- lib/config/offset_notifier.dart
+++ lib/config/offset_notifier.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:preload_page_view/preload_page_view.dart';
 
 class PageOffsetNotifier with ChangeNotifier {
-  double _offset = 0;
+  double _offset = 0; //0 to width size
   double _page = 0; //1 or 0
 
   PageOffsetNotifier(PreloadPageController pageController) {
